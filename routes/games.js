704eb6e57e4ca5b586f9b80d9c589e8cc1167407
Katie Blunck <97479562+katelyndb@users.noreply.github.com>
2023-03-10 13:53:20 -0700

    Testing

--- routes/games.js
+++ routes/games.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+// Error Handling and Validation
+const createError = require('http-errors');
+const path = require('path');
+const cors = require('cors');
+const { signupValidation, loginValidation } = require('../validation.js');
+//
 
 const gamesController = require('../controllers/games');
 // Gets all the games
@@ -14,3 +20,25 @@ router.put('/:id', gamesController.updateGameById);
 router.delete('/:id', gamesController.deleteGameById);
 
 module.exports = router;
+
+/*
+  app.post('/register', signupValidation, (req, res, next) => {
+    // your registration code
+ });
+  
+  
+ app.post('/login', loginValidation, (req, res, next) => {
+    // your login code
+ });
+  
+ // Handling Errors
+ app.use((err, req, res, next) => {
+     // console.log(err);
+     err.statusCode = err.statusCode || 500;
+     err.message = err.message || "Internal Server Error";
+     res.status(err.statusCode).json({
+       message: err.message,
+     });
+ });
+
+ */
--- server.js
+++ server.js
@@ -6,12 +6,7 @@ const bodyParser = require("body-parser");
 const mongodb = require("./db/connect");
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-// Error Handling and Validation
-const createError = require('http-errors');
-const path = require('path');
-const cors = require('cors');
-const { signupValidation, loginValidation } = require('./validation.js');
-//
+
 
 
 app
@@ -20,35 +15,13 @@ app
   .use(bodyParser.urlencoded({
     extended: true
   }))
-  use(cors())
-  // 
   .use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     next();
   })
   .use("/", require("./routes"));
 
-/*
-  app.post('/register', signupValidation, (req, res, next) => {
-    // your registration code
- });
-  
-  
- app.post('/login', loginValidation, (req, res, next) => {
-    // your login code
- });
-  
- // Handling Errors
- app.use((err, req, res, next) => {
-     // console.log(err);
-     err.statusCode = err.statusCode || 500;
-     err.message = err.message || "Internal Server Error";
-     res.status(err.statusCode).json({
-       message: err.message,
-     });
- });
 
- */
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   
